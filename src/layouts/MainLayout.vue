6b740a9ada572b8a65533367f008670c51abe38b
Chris Thomas Eikrem <chriseikrem97@gmail.com>
2021-07-21 13:10:44 +0200

    initial commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+    <div style="position: absolute; bottom: 0; right: 5px">
+      Data hentet fra FHI
+    </div>
+  </q-layout>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'MainLayout',
+  setup() {
+    return {};
+  },
+});
+</script>
